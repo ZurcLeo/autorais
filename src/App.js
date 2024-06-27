d28c5238eb63da70b41204b6879c4f954fdba296
ZurcLeo <leonardocruz.di@gmail.com>
2024-06-27 16:21:32 +0200

    atualizacao da pagina de entrada

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+
 import AppRoutes from './routes';
 import { Button, Container } from '@mui/material';
 
 const App = ({ toggleTheme }) => {
   return (
-    <Router>
+   
       <Container>
         <Button onClick={toggleTheme} variant="contained" color="primary" style={{ marginBottom: '1rem' }}>
           Toggle Theme
         </Button>
         <AppRoutes />
       </Container>
-    </Router>
+   
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,41 +2,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { UserProvider } from './components/resources/userContext';
-import { ConnectionProvider } from './components/resources/ConnectionService';
-import { StatusProvider } from './components/resources/StatusContext';
 import { ToastContainer } from 'react-toastify';
-import { PhotosProvider } from './components/resources/Common/PrivateRoute/hooks/PhotosContext';
 import App from './App';
-import AuthProvider from './components/resources/AuthService';
-import NotificationProvider from './components/resources/NotificationService';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import 'react-toastify/dist/ReactToastify.css';
-import './firebase.config';
+import './firebaseConfig';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <NotificationProvider>
-      <StatusProvider>
-        <BrowserRouter>
-          <AuthProvider>
-            <UserProvider>
-              <ConnectionProvider>
-                <PhotosProvider>
+<BrowserRouter>
                   <ToastContainer />
                   <App />
-                </PhotosProvider>
-              </ConnectionProvider>
-            </UserProvider>
-          </AuthProvider>
+              
         </BrowserRouter>
-      </StatusProvider>
-    </NotificationProvider>
+   
   </React.StrictMode>
 );
