c745c817f7e3d8042029efe30c3fd887b2c20a6d
ZurcLeo <leonardocruz.di@gmail.com>
2024-07-01 13:10:14 +0200

    Atualizando estrutura

--- src/components/customLinkContainer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const CustomLinkContainer = React.forwardRef((props, ref) => (
-  <Link ref={ref} {...props} />
-));
-
-export default CustomLinkContainer;
